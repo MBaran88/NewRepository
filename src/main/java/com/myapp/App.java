d4da4940a7f40588d147cc6ce9fb2a26f0699d51
MBaran88 <zdzibq@gmail.com>
2017-04-24 20:57:21 +0200

    commit#1

--- src/main/java/com/myapp/App.java
+++ src/main/java/com/myapp/App.java
@@ -4,6 +4,7 @@ import com.sun.org.apache.xpath.internal.SourceTree;
 
 /**
  * int res = (int)2.3f -> coś takiego zapisuje literał 2.3 i zapisuje go w formacie pamięci dostępnej dla int'a
+
  */
 public class App {
     public static void main(String[] args) {
@@ -11,8 +12,21 @@ public class App {
         float result = 4/3;
         System.out.println(result);
     }
+/** Metody są to funkcje które żyją w klasie np*/
 
+    public static double addTwoNumbers(double number1, double number2) {
 
+    double result = number1 + number2;
+
+    return
+
+    }
+    public static int methodOne(int arg1) {
+        return =0;
+    }
+    public static int methodTwo(int arg1){
+        return = 0;
+    }
 
 }
 
