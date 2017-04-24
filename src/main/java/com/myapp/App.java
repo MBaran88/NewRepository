8053f06521bc2a3867ef61fff73c1dc05e185cd8
MBaran88 <zdzibq@gmail.com>
2017-04-24 18:06:42 +0200

    commit#1

--- src/main/java/com/myapp/App.java
+++ src/main/java/com/myapp/App.java
@@ -1,13 +1,19 @@
 package com.myapp;
 
+import com.sun.org.apache.xpath.internal.SourceTree;
+
 /**
- * Hello world!
- *
+ * int res = (int)2.3f -> coś takiego zapisuje literał 2.3 i zapisuje go w formacie pamięci dostępnej dla int'a
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) {
+
+        float result = 4/3;
+        System.out.println(result);
     }
+
+
+
 }
+
+
