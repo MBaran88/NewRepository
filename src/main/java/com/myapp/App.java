98bfb6dd09f64376082f6ccee27eee63630e5e7d
MBaran88 <zdzibq@gmail.com>
2017-04-25 19:40:38 +0200

    commit#1

--- src/main/java/com/myapp/App.java
+++ src/main/java/com/myapp/App.java
@@ -4,30 +4,29 @@ import com.sun.org.apache.xpath.internal.SourceTree;
 
 /**
  * int res = (int)2.3f -> coś takiego zapisuje literał 2.3 i zapisuje go w formacie pamięci dostępnej dla int'a
-
  */
 public class App {
     public static void main(String[] args) {
 
-        float result = 4/3;
-        System.out.println(result);
-    }
-/** Metody są to funkcje które żyją w klasie np*/
-
-    public static double addTwoNumbers(double number1, double number2) {
-
-    double result = number1 + number2;
-
-    return
-
-    }
-    public static int methodOne(int arg1) {
-        return =0;
-    }
-    public static int methodTwo(int arg1){
-        return = 0;
-    }
-
-}
+        int limit = 100;
 
 
+        int[] tablica = new int[100];
+        for (int i = 0; i < 99; i++)
+            tablica[i] = i + 1;
+
+        int zmienna = tablica[3];
+        int suma = 0;
+
+        for (int i = 0; i < 100; i++) {
+            suma = suma + tablica[i];
+
+        }
+        System.out.println(suma/100d);
+
+
+    }}
+
+     /** tablica z elementami od 0 do 99
+     zrobic metode ktora zwroci nam srednia arytmetyczna
+     100 intow**/
