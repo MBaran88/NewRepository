95735e91132c3ae075ec406a96de26413ffd5482
MBaran88 <zdzibq@gmail.com>
2017-04-25 20:59:27 +0200

    commit#1

--- src/main/java/com/myapp/App.java
+++ src/main/java/com/myapp/App.java
@@ -8,14 +8,10 @@ import com.sun.org.apache.xpath.internal.SourceTree;
 public class App {
     public static void main(String[] args) {
 
-        int limit = 100;
-
-
         int[] tablica = new int[100];
         for (int i = 0; i < 99; i++)
             tablica[i] = i + 1;
 
-        int zmienna = tablica[3];
         int suma = 0;
 
         for (int i = 0; i < 100; i++) {
@@ -25,7 +21,12 @@ public class App {
         System.out.println(suma/100d);
 
 
-    }}
+    }
+    public static (String[] args) {
+
+        Car myCar1 = new Car();
+        System.out.println(myCar1.toString());
+}}
 
      /** tablica z elementami od 0 do 99
      zrobic metode ktora zwroci nam srednia arytmetyczna
---
+++ src/main/java/com/myapp/Wheel.java
@@ -0,0 +1,7 @@
+package com.myapp;
+
+/**
+ * Created by RENT on 2017-04-25.
+ */
+public class Wheel {
+}
